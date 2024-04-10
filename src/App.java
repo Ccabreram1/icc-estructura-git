1d5be233a9ad83c145d23df186e430f7a5e3e33d
Silvio <silviopinosastudillo@gmail.com>
2024-04-10 12:39:23 -0500

    ADD: Metodo bubblesort

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ///Crear la instancia de la (calse u objeto) MetodosOrdenamiento
+        MetodosOrdenamiento metodosOrdenamiento = new MetodosOrdenamiento();
+        //Creo el arreglo de enteros
+        int[] numeros ={30, 7, 12, 5};
+
+        //Mandar a imprimir mi arreglo
+        metodosOrdenamiento.imprime(numeros);
+
+
+        //Llamo al metodo sortBybubble y su respuesta guardo en la variable arregloOrdenadoBuble
+        int[] arregloOrdenadoBuble = metodosOrdenamiento.sortByBubnle(numeros,false);
+        //Mando a imprimir mi nuevo arreglo que ya fue ordenado en arregloOrdenadoBuble 
+        metodosOrdenamiento.imprime(arregloOrdenadoBuble);
+
+
+
+
+        ///TODO: Hacer un menu que elija el metodo
     }
 }
---
+++ src/MetodosOrdenamiento.java
@@ -0,0 +1,56 @@
+public class MetodosOrdenamiento {
+    //1. Metodo que devuelve un arreglo de enteros ordenados por Burbuja 
+    public int[] sortByBubnle(int[]arreglo,boolean longs){
+        ///Code to sort 
+        /// Obtener el tamaño del arreglo
+        int tamano = arreglo.length;
+
+        ///Bucle externo que va a recorrer todo el arreglo
+        
+        for (int i = 0;i<tamano;i++){
+            if(longs){
+                System.out.println("\nPasado numero " +i);
+            }
+           
+
+            //Blucle for interno que compara el elemento actual con los siguientes
+            for (int j = i+1;j<tamano;j++){
+                if (longs) {
+                    System.out.println("i="+arreglo[i]+"j="+arreglo[j]);
+                }
+                if(arreglo[i]>arreglo[j]){
+                    //Intercambiamos los elementos
+                    if (longs) {
+                        System.out.println("-Si es mayor asi de cambio ");
+                    }
+                    int temporal = arreglo[i];
+                    arreglo[i]=arreglo[j];
+                    arreglo[j]=temporal;
+
+                    if (longs) {
+                        System.out.print(" ");
+                        imprime(arreglo);
+                    }
+                }
+            }
+        }
+        return arreglo;
+    }
+
+    //2. Metodo que devuelve un arreglo de enteros ordenados por Seleccion
+    
+    
+    //3. Metodo que devuelve un arreglo de enteros ordenados por Insercion
+
+    //4. Metodo que devuelve un arreglo de enteros ordenados por 
+    public void imprime(int[] arreglo){
+        for (int i = 0; i<arreglo.length; i++){
+            System.out.print(arreglo[i]+ " ");
+        }
+        System.out.println();
+    }
+
+
+
+    
+}
